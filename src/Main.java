2e02ccd3fd8bc0f43f4637471d447631497a7ad5
LORD OF GAME <59160969@go.buu.ac.th>
2018-08-29 13:40:12 +0700

    ทำกล่องตอบข้อความ ลบกล่องข้อมูลเปล่า

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog("ใส่ตัวเลข 1 ตัว","0");
-        JOptionPane.showMessageDialog(null,"message","title",JOptionPane.INFORMATION_MESSAGE);
         JOptionPane.showMessageDialog(null,input,"ค่าที่ใส่",JOptionPane.INFORMATION_MESSAGE);
        int  ans = JOptionPane.showConfirmDialog(null,"รักหรือเปล่า","คำถาม",JOptionPane.YES_NO_OPTION);
        if(ans==JOptionPane.YES_NO_OPTION){
